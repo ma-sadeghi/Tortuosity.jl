6c70ae35cd203092da070fc2c67829df781b6f44
Amin Sadeghi <amin.sadeghi@live.com>
2023-09-03 08:27:20 -0400

    Add helper to parse CLI args

--- src/utils.jl
+++ src/utils.jl
@@ -58,4 +58,13 @@ function calc_tortuosity(c, template)
 end
 
 
+function parse_args(s::String)
+    # Parse command-line arguments in the form of "--key=value"
+    regex = r"--(\w+)=([^\s]+)"    
+    matches = eachmatch(regex, s)
+    pairs = Dict(m.captures[1] => m.captures[2] for m in matches)
+    return pairs
+end
+
+
 nothing
