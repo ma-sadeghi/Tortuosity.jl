93e80ac2f4b01bc4020b3f7a0a5886a01702d5b0
Amin Sadeghi <amin.sadeghi@live.com>
2023-09-18 04:07:28 -0400

    Add more plotting helpers

--- src/plottools.jl
+++ src/plottools.jl
@@ -1,7 +1,30 @@
 using Plots
 
 
-function imshow(im; z_idx=1)
-    im_slice = ndims(im) == 3 ? im[:, :, z_idx] : im
-    heatmap(im_slice, aspect_ratio=:equal, color=:viridis, clim=(0, 1))
+function set_plotsjl_defaults()
+    default()
+    default(size=(600, 400), thickness_scaling=1.25)
+    scalefontsizes(1.25)
+end
+
+
+# function imshow(im; z_idx)
+#     im_slice = ndims(im) == 3 ? im[:, :, z_idx] : im
+#     heatmap(im_slice, aspect_ratio=:equal, color=:viridis, clim=(0, 1))
+# end
+
+
+function imshow(im; vals=nothing, z_idx=1)
+    fillval = isa(vals, Nothing) ? 0 : NaN
+    u = fill(fillval, size(im))
+    u[im] .= isa(vals, Nothing) ? 1 : Array(vals)
+    u_slice = ndims(im) == 3 ? u[:, :, z_idx] : u
+    heatmap(u_slice, aspect_ratio=:equal, color=:viridis, clim=(0, 1))
+end
+
+
+function reshape(u, im)
+    c = fill(NaN, size(im))
+    c[im] = Array(u)
+    return c
 end
