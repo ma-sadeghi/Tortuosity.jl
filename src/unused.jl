03ed9a2e5ae4559e7f0b8e6985b3e1555ff6afc5
Amin Sadeghi <amin.sadeghi@live.com>
2023-09-18 13:33:03 -0400

    Move read_linear_sys to unused.jl

---
+++ src/unused.jl
@@ -0,0 +1,27 @@
+using HDF5
+
+
+function read_linear_sys(path; sparse_fmt)
+    @assert sparse_fmt in ["csc", "coo"]
+    fid = h5open(path, "r")
+
+    # Load common attributes
+    shape = Tuple(read(fid["shape"]))
+    template = Bool.(read(fid["template"]))
+    rhs = read(fid["rhs"])
+    nzval = read(fid["nzval"])
+
+    # Load matrix-specific attributes
+    if sparse_fmt == "csc"
+        # NOTE: Convert to 1-based indexing
+        colptr = read(fid["colptr"]) .+ 1
+        rowval = read(fid["rowval"]) .+ 1
+        spmat_args = (colptr, rowval, nzval, shape)
+    elseif sparse_fmt == "coo"
+        row = read(fid["row"]) .+ 1
+        col = read(fid["col"]) .+ 1
+        spmat_args = (row, col, nzval, shape)
+    end
+
+    return spmat_args, rhs, template
+end
