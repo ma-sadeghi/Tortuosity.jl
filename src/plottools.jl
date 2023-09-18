b80a0a5773f953884c4635a862e1ac7126c1a8b9
Amin Sadeghi <amin.sadeghi@live.com>
2023-09-18 13:29:27 -0400

    Simplify imshow helper

--- src/plottools.jl
+++ src/plottools.jl
@@ -8,19 +8,19 @@ function set_plotsjl_defaults()
 end
 
 
-# function imshow(im; z_idx)
-#     im_slice = ndims(im) == 3 ? im[:, :, z_idx] : im
-#     heatmap(im_slice, aspect_ratio=:equal, color=:viridis, clim=(0, 1))
-# end
-
-
-function imshow(im; vals=nothing, z_idx=1)
-    fillval = isa(vals, Nothing) ? 0 : NaN
-    u = fill(fillval, size(im))
-    u[im] .= isa(vals, Nothing) ? 1 : Array(vals)
-    u_slice = ndims(im) == 3 ? u[:, :, z_idx] : u
-    heatmap(u_slice, aspect_ratio=:equal, color=:viridis, clim=(0, 1))
+function imshow(im; slice=1)
+    im_slice = ndims(im) == 3 ? im[:, :, slice] : im
+    heatmap(im_slice, aspect_ratio=:equal, color=:viridis, clim=(0, 1))
 end
+
+
+# function imshow(im; vals=nothing, slice=1)
+#     fillval = isa(vals, Nothing) ? 0 : NaN
+#     u = fill(fillval, size(im))
+#     u[im] .= isa(vals, Nothing) ? 1 : Array(vals)
+#     u_slice = ndims(im) == 3 ? u[:, :, slice] : u
+#     heatmap(u_slice, aspect_ratio=:equal, color=:viridis, clim=(0, 1))
+# end
 
 
 function vec_to_field(u, im)
