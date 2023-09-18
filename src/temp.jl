dcc50bf4cb0adb4f64832ee0140a9814226ecc26
Amin Sadeghi <amin.sadeghi@live.com>
2023-09-18 13:37:14 -0400

    Add WIP prototype for using AlgebraicMultigrid as solver

--- src/temp.jl
+++ src/temp.jl
@@ -1,37 +1,29 @@
+using AlgebraicMultigrid
 using LinearSolve
 using Plots
 using Random
 
+includet("dnstools.jl")
 includet("imgen.jl")
+includet("numpytools.jl")
 includet("pdetools.jl")
+includet("plottools.jl")
+includet("simulations.jl")
 includet("topotools.jl")
 
+
 Random.seed!(2)
-shape = (256, 256)
-porosity = 0.65
-blobiness = 1
-gd = 1.0
 
-im = blobs(shape, porosity, blobiness)
-im = denoise(im, 2)
-display(heatmap(im, aspect_ratio=:equal, color=:viridis, clim=(0, 1)))
+img = blobs(shape=(64, 64, 64), porosity=0.65, blobiness=1)
+img = denoise(img, 2)
+display(imshow(img, slice=1))
 
-nnodes = sum(im)
-conns = create_connectivity_list(im)
-am = create_adjacency_matrix(conns, nnodes=nnodes, weights=gd)
-A = laplacian(am)
-b = zeros(nnodes)
+prob = tortuosity_fdm(img, axis=:x)
+sol_cg = solve(prob, KrylovJL_CG(), verbose=true, reltol=1e-5)
+sol_amg = solve(prob.A, prob.b, RugeStubenAMG(), maxiter=1000, reltol=1e-5)
 
-left = find_boundary_nodes(im, :left)
-right = find_boundary_nodes(im, :right)
+c_cg = vec_to_field(sol_cg.u, img)
+c_amg = vec_to_field(sol_amg, img)
 
-apply_dirichlet_bc!(A, b, bc_nodes=left, bc_values=1.0)
-apply_dirichlet_bc!(A, b, bc_nodes=right, bc_values=0.0)
-
-prob = LinearProblem(A, b)
-sol = solve(prob, KrylovJL_CG(), verbose=true, reltol=1e-6)
-
-c = zeros(size(im)) * NaN
-c[im] = Array(sol.u)
-
-display(heatmap(c[:, :, 1], aspect_ratio=:equal, color=:viridis, clim=(0, 1)))
+display(imshow(c_cg, slice=1))
+display(imshow(c_amg, slice=1))
