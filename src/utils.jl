9ffa4fbf2bba552cb1b2ce5eb0be70fe452582fa
Amin Sadeghi <amin.sadeghi@live.com>
2023-09-19 12:36:52 -0400

    Add helper to parse CLI args

--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,16 @@
-function parse_args(s::String)
+function args_to_dict(args)
+    s = join(args, " ")
     # Parse command-line arguments in the form of "--key=value"
     regex = r"--(\w+)=([^\s]+)"
     matches = eachmatch(regex, s)
     pairs = Dict(m.captures[1] => m.captures[2] for m in matches)
     return pairs
+end
+
+
+function format_args_dict(args_dict)
+    fpath = args_dict["fpath"]
+    path_export = args_dict["path_export"]
+    gpu_id = parse(Int, args_dict["gpu_id"])
+    return fpath, path_export, gpu_id
 end
