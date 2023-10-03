569ce6f60babc612f8aada95b6983c0159684556
Amin Sadeghi <amin.sadeghi@live.com>
2023-10-03 11:15:13 -0400

    Add unit tests for phase_fraction

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,10 @@ using Test
 
 @testset verbose=true "Tortuosity.jl" begin
 
+    @testset verbose=true "Utility functions" begin
+        include("test_utils.jl")
+    end
+
     @testset verbose=true "Basic geometries" begin
         include("test_basic.jl")
     end
---
+++ test/test_utils.jl
@@ -0,0 +1,35 @@
+using Test
+using Tortuosity
+
+# Set up fixtures
+img = ones(UInt8, (32, 32, 32))
+img[:, :, 1:5] .= 0
+img[:, :, 21:32] .= 5
+ε0 = 32*32*5 / (32*32*32)
+ε1 = 32*32*15 / (32*32*32)
+ε5 = 32*32*12 / (32*32*32)
+
+@testset verbose=true "phase_fraction" begin
+
+    @testset "No labels passed" begin
+        fracs = phase_fraction(img)
+        @test fracs[0] ≈ ε0 atol=1e-4
+        @test fracs[1] ≈ ε1 atol=1e-4
+        @test fracs[5] ≈ ε5 atol=1e-4
+        @test sum(values(fracs)) ≈ 1.0 atol=1e-4
+    end
+
+    @testset "Single label passed" begin
+        @test phase_fraction(img, 0) ≈ ε0 atol=1e-4
+        @test phase_fraction(img, 1) ≈ ε1 atol=1e-4
+        @test phase_fraction(img, 5) ≈ ε5 atol=1e-4
+    end
+
+    @testset "Multiple labels passed" begin
+        @test phase_fraction(img, [0, 1]) ≈ ε0 + ε1 atol=1e-4
+        @test phase_fraction(img, [1, 5]) ≈ ε1 + ε5 atol=1e-4
+        @test phase_fraction(img, [0, 5]) ≈ ε0 + ε5 atol=1e-4
+        @test phase_fraction(img, [0, 1, 5]) ≈ ε0 + ε1 + ε5 atol=1e-4
+    end
+
+end
