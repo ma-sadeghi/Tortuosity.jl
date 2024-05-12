a9897be353a5f35ff68aef5476d52cf75ecd2e17
Amin Sadeghi <amin.sadeghi@live.com>
2024-05-12 10:29:22 -0400

    test: Run a random test image on `porespy.tortuosity_fd` and store results as jld2 file for reproducibility

--- test/test_blobs.jl
+++ test/test_blobs.jl
@@ -1,36 +1,64 @@
+using JLD2
 using LinearSolve
-using NPZ
 using Test
 using Tortuosity
 
+# ---------------------------------------- #
+# Generate test data
+# ---------------------------------------- #
+
+# using PythonCall
+# op = pyimport("openpnm")
+# ps = pyimport("porespy")
+
+# img = ps.generators.blobs(; shape=(32, 42, 52), porosity=0.7, blobiness=[0.5, 1, 2])
+# img = Imaginator.trim_nonpercolating_paths(img, :x)
+# img = Imaginator.trim_nonpercolating_paths(img, :y)
+# img = Imaginator.trim_nonpercolating_paths(img, :z)
+
+# pardiso = op.solvers.PardisoSpsolve()
+# res = Dict(
+#     axis => ps.simulations.tortuosity_fd(Py(img).to_numpy(); axis=i - 1, solver=pardiso) for
+#     (i, axis) in enumerate([:x, :y, :z])
+# )
+
+# tau = Dict(axis => pyconvert(Float64, res[axis].tortuosity) for axis in [:x, :y, :z])
+# c̄ = Dict(
+#     axis => mean(pyconvert(Array{Float64}, res[axis].concentration)[img]) for
+#     axis in [:x, :y, :z]
+# )
+# jldsave("blobs.jld2"; img=img, tau=tau, c̄=c̄)
+
+# ---------------------------------------- #
+
 # Set up fixtures
-img = Imaginator.blobs(shape=(32, 32, 32), porosity=0.65, blobiness=0.5, seed=2)
+test_data = load("blobs.jld2")
+img = test_data["img"]
+tau_gt = test_data["tau"]
+c̄_gt = test_data["c̄"]
 ε = phase_fraction(img, 1)
 # Build diffusivity matrix
 D = fill(NaN, size(img))
 D[img] .= 1.0               # Fluid phase
 D[.!img] .= 1e-4            # Solid phase
-# Ground truth values for blobs
-tau_gt = Dict(:x => 1.637568, :y => 1.501668, :z => 1.564219)
-c̄_gt = Dict(:x => 0.447173, :y => 0.509658, :z => 0.461578)
 
 @testset "Blobs 3D, $(ax)-axis" for ax in (:x, :y, :z)
-    sim = TortuositySimulation(img, axis=ax, gpu=false)
-    sol = solve(sim.prob, KrylovJL_CG(), reltol=1e-6)
+    sim = TortuositySimulation(img; axis=ax, gpu=false)
+    sol = solve(sim.prob, KrylovJL_CG(); reltol=1e-6)
     c̄ = mean(sol.u)
-    @test c̄ ≈ c̄_gt[ax] atol=1e-4
+    @test c̄ ≈ c̄_gt[ax] atol = 1e-4
     c_grid = vec_to_field(sol.u, img)
     tau = tortuosity(c_grid, ax)
-    @test tau ≈ tau_gt[ax] atol=1e-4
+    @test tau ≈ tau_gt[ax] atol = 1e-4
 end
 
 @testset "Blobs 3D, variable diffusivity, $(ax)-axis" for ax in (:x, :y, :z)
     domain = ones(Bool, size(img))
-    sim = TortuositySimulation(domain, axis=ax, gpu=false, D=D)
-    sol = solve(sim.prob, KrylovJL_CG(), reltol=1e-6)
+    sim = TortuositySimulation(domain; axis=ax, gpu=false, D=D)
+    sol = solve(sim.prob, KrylovJL_CG(); reltol=1e-6)
     c̄ = mean(sol.u[img[:]])
-    @test c̄ ≈ c̄_gt[ax] atol=1e-2
+    @test c̄ ≈ c̄_gt[ax] atol = 1e-2
     c_grid = vec_to_field(sol.u, domain)
-    tau = tortuosity(c_grid, ax, eps=ε, D=D)
-    @test tau ≈ tau_gt[ax] atol=1e-2
+    tau = tortuosity(c_grid, ax; eps=ε, D=D)
+    @test tau ≈ tau_gt[ax] atol = 1e-2
 end
