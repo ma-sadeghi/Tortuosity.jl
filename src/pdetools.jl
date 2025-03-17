8fa4edbe4f06d579753717ec8a079a80cb77c6e9
Amin Sadeghi <amin.sadeghi@live.com>
2025-03-17 07:12:04 -0700

    feat: Add `apply_dirichlet_bc🚀!` for `CUDA.CUSPARSE.CuSparseMatrixCSC`

--- src/pdetools.jl
+++ src/pdetools.jl
@@ -31,3 +31,20 @@ function apply_dirichlet_bc🚀!(A::SparseMatrixCSC, b; nodes, vals)
     b[nodes] .= vals .* diag_vals
     return dropzeros!(A)
 end
+
+function apply_dirichlet_bc🚀!(A::CUDA.CUSPARSE.CuSparseMatrixCSC, b; nodes, vals)
+    diag_inds = CUDA.diagind(A)[nodes]
+    diag_vals = CUDA.diag(A)[nodes]
+    # Add contribution from BCs to the RHS
+    x_bc = multihotvec(nodes, length(b); vals=vals)
+    b .-= A * x_bc
+    # Zero out rows and columns corresponding to BCs
+    I, J, _ = CUDA.CUSPARSE.findnz(A)
+    row_inds = overlap_indices🚀(I, nodes)
+    col_inds = overlap_indices🚀(J, nodes)
+    A.nzval[union(row_inds, col_inds)] .= 0.0
+    # Ensure Dirichlet BCs are satisfied
+    A[diag_inds] .= diag_vals
+    b[nodes] .= vals .* diag_vals
+    return dropzeros!(A)
+end
