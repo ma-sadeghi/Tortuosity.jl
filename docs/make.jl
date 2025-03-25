439ba981a3b8a4c65d35b479984956b4d842a718
Amin Sadeghi <amin.sadeghi@live.com>
2025-03-25 15:45:59 -0700

    ci: update documentation deployment settings

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,18 @@
-using Documenter, Tortuosity
+using Documenter
+using Tortuosity
 
 format = Documenter.HTML(;
     edit_link="main",
-    # prettyurls=get(ENV, "CI", nothing) == "true",
+    prettyurls=get(ENV, "CI", nothing) == "true",
     assets=[joinpath("assets", "style.css")],
 )
 
 makedocs(; sitename="Tortuosity.jl", format=format)
+
+deploydocs(;
+    repo="github.com/ma-sadeghi/Tortuosity.jl.git",
+    versions=["stable" => "v^", "v#.#.#", "dev" => "dev"],
+    forcepush=true,
+    push_preview=true,
+    devbranch="main",
+)
