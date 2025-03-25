a8011f8875b5f006913d570d308d3dc70447d729
Amin Sadeghi <amin.sadeghi@live.com>
2025-03-25 00:34:17 -0700

    refactor: rename `vec_to_field` to `vec_to_grid` and update usage

--- examples/demo_basic.jl
+++ examples/demo_basic.jl
@@ -1,5 +1,7 @@
 # %% Imports
 using Tortuosity
+using Tortuosity: TortuositySimulation, tortuosity, formation_factor, vec_to_grid
+
 using CUDA
 using LinearSolve
 using Plots
@@ -7,18 +9,19 @@ using Printf
 using BenchmarkTools
 
 # %% Generate/load the image
-img = Imaginator.blobs(shape=(64, 64, 1), porosity=0.65, blobiness=0.5, seed=2);
-display(heatmap(img[:, :, 1], aspect_ratio=:equal, clim=(0, 1)))
+show_plots = false
+img = Imaginator.blobs(; shape=(64, 64, 1), porosity=0.65, blobiness=0.5, seed=2);
+show_plots && display(heatmap(img[:, :, 1]; aspect_ratio=:equal, clim=(0, 1)));
 
 # %% Build Ax = b on CPU/GPU
-sim = TortuositySimulation(img, axis=:x, gpu=false)
+sim = TortuositySimulation(img; axis=:x, gpu=false);
 
 # %% Solve Ax = b using an iterative solver
-@time sol = solve(sim.prob, KrylovJL_CG(), verbose=false, reltol=eltype(sim.prob.b)(1e-5));
+@time sol = solve(sim.prob, KrylovJL_CG(); verbose=false, reltol=1e-5);
 
 # %% Compute the tortuosity factor and visualize the solution
-c_grid = vec_to_field(sol.u, img)
+c_grid = vec_to_grid(sol.u, img)
 τ = tortuosity(c_grid, :x)
 F = formation_factor(c_grid, :x)
 @info "τ: $(@sprintf("%.5f", τ)), ℱ: $(@sprintf("%.5f", F))"
-display(heatmap(c_grid[:, :, 1], aspect_ratio=:equal, clim=(0, 1)))
+show_plots && display(heatmap(c_grid[:, :, 1]; aspect_ratio=:equal, clim=(0, 1)));
