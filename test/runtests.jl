d60d935b6e136b2bb05923517f1eb72f09d98a5d
Amin Sadeghi <amin.sadeghi@live.com>
2025-03-25 15:12:04 -0700

    chore: clean up unused dependencies, go back to [targets] as opposed to separate test/Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,16 @@
-using Tortuosity
 using Test
+using Tortuosity
 
-@testset verbose=true "Tortuosity.jl" begin
-
-    @testset verbose=true "Utility functions" begin
+@testset verbose = true "Tortuosity.jl" begin
+    @testset verbose = true "Utility functions" begin
         include("test_utils.jl")
     end
 
-    @testset verbose=true "Basic geometries" begin
+    @testset verbose = true "Basic geometries" begin
         include("test_basic.jl")
     end
 
-    @testset verbose=true "Blobs (Gaussian noise)" begin
+    @testset verbose = true "Blobs (Gaussian noise)" begin
         include("test_blobs.jl")
     end
-
 end
