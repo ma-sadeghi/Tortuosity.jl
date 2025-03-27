94cdfa458018d9bb14cdd07fde07b7a3fbd7b676
Amin Sadeghi <amin.sadeghi@live.com>
2025-03-27 02:08:40 -0700

    refactor: export `solve` and `KrylovJL_CG` for convenience + update demo script

--- examples/demo_basic.jl
+++ examples/demo_basic.jl
@@ -1,16 +1,14 @@
 # %% Imports
-using BenchmarkTools
-using CUDA
-using LinearSolve
 using Plots
 using Printf
 using Tortuosity
-using Tortuosity: TortuositySimulation, formation_factor, tortuosity, vec_to_grid
+using Tortuosity: Imaginator, TortuositySimulation, tortuosity, vec_to_grid
 
 # %% Generate/load the image
 show_plots = false
 gpu = false
-img = Imaginator.blobs(; shape=(64, 64, 1), porosity=0.65, blobiness=0.5, seed=2);
+img = Imaginator.blobs(; shape=(64, 64, 1), porosity=0.5, blobiness=1, seed=2);
+img = Imaginator.trim_nonpercolating_paths(img, :x);
 show_plots && display(heatmap(img[:, :, 1]; aspect_ratio=:equal, clim=(0, 1)));
 
 # %% Build Ax = b on CPU/GPU
@@ -23,5 +21,5 @@ sim = TortuositySimulation(img; axis=:x, gpu=gpu);
 c_grid = vec_to_grid(sol.u, img)
 τ = tortuosity(c_grid, :x)
 F = formation_factor(c_grid, :x)
-@info "τ: $(@sprintf("%.5f", τ)), ℱ: $(@sprintf("%.5f", F))"
+@info "τ: $(@sprintf("%.5f", τ)))"
 show_plots && display(heatmap(c_grid[:, :, 1]; aspect_ratio=:equal, clim=(0, 1)));
