6641cefe6fe39c8554af70e65b38180de18fcf42
Amin Sadeghi <amin.sadeghi@live.com>
2025-06-09 18:10:13 -0400

    refactor(dnstools, topotools): replace '===' with 'isnothing' for better readability

--- src/dnstools.jl
+++ src/dnstools.jl
@@ -9,17 +9,17 @@ function effective_diffusivity(
     scalar_field; axis, slice=1, D=nothing, L=nothing, Δc=nothing
 )
     axis_idx = Dict(:x => 1, :y => 2, :z => 3)[axis]
-    L = L === nothing ? size(scalar_field)[axis_idx] : L
+    L = isnothing(L) ? size(scalar_field)[axis_idx] : L
     # NOTE: If L is provided, don't use it to compute A since it might not be the actual L
     A = prod(size(scalar_field)) / size(scalar_field, axis_idx)
     c₁ = nanmean(selectdim(scalar_field, axis_idx, 1))
     c₂ = nanmean(selectdim(scalar_field, axis_idx, size(scalar_field)[axis_idx]))
-    Δc = Δc === nothing ? c₁ - c₂ : Δc
+    Δc = isnothing(Δc) ? c₁ - c₂ : Δc
 
     # Extract the conductance of first and second slices if D is provided
     # NOTE: voxel size = 1 => gd = D⋅A/ℓ = D
-    D1 = D === nothing ? 1.0 : selectdim(D, axis_idx, slice)
-    D2 = D === nothing ? 1.0 : selectdim(D, axis_idx, slice + 1)
+    D1 = isnothing(D) ? 1.0 : selectdim(D, axis_idx, slice)
+    D2 = isnothing(D) ? 1.0 : selectdim(D, axis_idx, slice + 1)
     D12 = 1 ./ (0.5 ./ D1 + 0.5 ./ D2)
 
     first_slice = selectdim(scalar_field, axis_idx, slice)
@@ -31,7 +31,7 @@ end
 
 function tortuosity(c; axis, slice=1, eps=nothing, D=nothing, L=nothing, Δc=nothing)
     # NOTE: Assumes that c is NaN-filled outside the pore space
-    ε = eps === nothing ? sum(isfinite.(c)) / prod(size(c)) : eps
+    ε = isnothing(eps) ? sum(isfinite.(c)) / prod(size(c)) : eps
     Deff = effective_diffusivity(c; axis=axis, slice=slice, D=D, L=L, Δc=Δc)
     return ε / Deff
 end
