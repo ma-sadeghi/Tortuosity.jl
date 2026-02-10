6a9ccf9c383caa761365c8fb1a060c0c8b6e7ac9
Sawyer <shossfel@uwaterloo.ca>
2026-02-10 16:39:38 -0500

    refactor: extract measurement functions to transient_measurements.jl

---
+++ src/transient_measurements.jl
@@ -0,0 +1,114 @@
+##--- functions for extracting datapoints of interest from concentration distributions or problem---
+
+function porosity(porosity_mask)
+    sum(porosity_mask)/length(porosity_mask) #assumes pores are represented by 1, not very standard but that is the definition here
+end
+porosity(problem::DiffusionProblem) = porosity(problem.porosity_mask)
+
+
+function slice_conc_dist(C, mask, axis)
+
+    collapse = AXIS_COMPLEMENT[axis]
+
+    return dropdims(sum(C, dims = collapse)./sum(mask, dims=collapse), dims = collapse)
+end
+slice_conc_dist(C, prob::DiffusionProblem) = slice_conc_dist(C, prob.porosity_mask, prob.axis)
+
+
+"""
+returns average concentration of pores in slices perpendicular to axis at index(es) ind
+
+"""
+function get_slice_conc(C, mask, axis, ind)
+    ax = AXIS_DEFINITION[axis]
+
+    C_slice = selectdim(C, ax, ind)
+    mask_slice = selectdim(mask, ax, ind)
+
+    return sum(C_slice)/sum(mask_slice)
+end
+get_slice_conc(C, prob::DiffusionProblem, ind) = get_slice_conc(C, prob.porosity_mask, prob.axis, ind)
+
+
+
+"""
+flux_dist(C, prob)
+input the C distribution for a timestep
+
+input C, and either dx or the associated DiffusionProblem
+returns a vector of the flux between each 2d slice of voxels along direction of axis or problem.axis
+    or just between ind and ind+1 for entries to inds
+"""
+function flux_dist(C, D, dx, mask, axis; inds = nothing)
+    ax = AXIS_DEFINITION[axis]      # 1, 2, or 3
+    N  = size(C, ax)
+
+    # all slice pairs (1,2), (2,3), ..., (N-1,N)
+    isnothing(inds) && (inds = 1:(N-1)) #default to entire distribution
+
+    # accumulate flux contributions for each slice pair
+    fluxes = similar(inds, Float64)
+
+    for (k, i) in enumerate(inds)
+        C1 = selectdim(C, ax, i)
+        C2 = selectdim(C, ax, i+1)
+
+        m1 = selectdim(mask, ax, i)
+        m2 = selectdim(mask, ax, i+1)
+
+        
+        ΔC = C1 .* (m2 .!= 0) .- C2 .* (m1 .!= 0)
+        
+        # sum over the perpendicular axes
+        fluxes[k] = sum(ΔC) .* (D * dx) # D/dx *dx^2 = D/dx * A/voxel
+    end
+
+    return fluxes
+end
+flux_dist(C, prob::DiffusionProblem; inds = nothing)=  flux_dist(C, prob.D_free, prob.dx, prob.porosity_mask, prob.axis; inds = inds)
+
+
+"""
+returns flux between slice of C at ind and ind+1
+"""
+function get_flux(C::Array, D, dx, mask, axis; ind=:end)
+    ax = AXIS_DEFINITION[axis]          # 1, 2, or 3
+    N  = size(C, ax)
+
+    ind === :end && (ind = N - 1) #end symbol for flux between second last and last slice
+
+    # slices along the chosen axis
+    C1 = selectdim(C, ax, ind)
+    C2 = selectdim(C, ax, ind + 1)
+
+    m1 = selectdim(mask, ax, ind)
+    m2 = selectdim(mask, ax, ind + 1)
+
+    # flux only through pore voxels
+    ΔC = C1 .* (m2 .!= 0) .- C2 .* (m1 .!= 0)
+
+    # sum over the two perpendicular axes
+    flux = sum(ΔC) #, dims = AXIS_COMPLEMENT[ax])
+
+    return flux * (D * dx) #D/dx *dx^2
+end
+get_flux(C, prob::DiffusionProblem; ind=:end)=  get_flux(C, prob.D_free, prob.dx, prob.porosity_mask, prob.axis; ind=ind)
+
+"""
+normalized_mass_intake(state; eql_intake = nothing)
+
+return the time curve for total mass intake since initial-conditions, normalized to the dimensionless range (0,1)
+
+#Arguments
+    state: a DiffusionState holding the solved datapoints
+    eql_intake: the mass_intake at steady state, defaults to the mass intake at the last step in C_history
+        *if eql_intake not inputted and the last step in C_history is not at steady state, the output is not useable
+"""
+function normalized_mass_intake(state; eql_intake = nothing)
+
+    if eql_intake === nothing
+        eql_intake = sum(state.C[end])-sum(state.C[1])
+    end
+    mass_intake = A -> (sum(A)-sum(state.C[1]))/eql_intake
+    return map(mass_intake, state.C)
+end
