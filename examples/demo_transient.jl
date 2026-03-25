931c72cde0ff3a9028affa49af70aeec7a367bdf
Sawyer <shossfel@uwaterloo.ca>
2026-03-25 16:46:26 -0400

    made homogenous transient fitting less confusing, removed extra degree of freedom to only fit tortuosity (fixing steady state values to steady state solution could work better)

--- examples/demo_transient.jl
+++ examples/demo_transient.jl
@@ -4,26 +4,26 @@
 using Plots
 using Printf
 using Tortuosity
-using Tortuosity: Imaginator, stop_at_delta_flux, effective_diffusivity, analytic_mass
+using Tortuosity: stop_at_delta_flux, effective_diffusivity, analytic_mass
 
-PLOT = false
+PLOT = true
 USE_GPU = false
 
 # %% ------------------------------------------------------
 # Generate/load the image
 
 shape = (64, 64, 64)
-img = Imaginator.blobs(; shape=shape, porosity=0.5, blobiness=1, seed=2);
+img = Imaginator.blobs(; shape=shape, porosity=0.65, blobiness=1, seed=2);
 img = Imaginator.trim_nonpercolating_paths(img; axis=:x);
 PLOT && display(heatmap(img[:, :, shape[3] ÷ 2]; aspect_ratio=:equal, clim=(0, 1)));
 
 # %% ------------------------------------------------------
 # Build A*C = dC on CPU/GPU and run the transient solver until it reaches stop_condition
 # Save a data point every dt time units in simulation time
-dt = 0.05
+dt = 0.1
 
 problem = TransientProblem(img, dt; axis = :x, gpu = USE_GPU,
-    bound_mode=(1,0)
+    bound_mode=(1,0) # referring to Dirichlet bound at inlet and outlet
 )
 sim = init_state(problem);
 
@@ -33,21 +33,19 @@ solve!(sim, problem, stop_condition)
 
 # %% ------------------------------------------------------
 # Compute the tortuosity factor and visualize the solution
-# very much a work in progress part
 
-D_eff_pore, φ, xdata, ydata, fit, f = effective_diffusivity(sim, problem, :mass)
+τ, D_eff, xdata, ydata, fit, f = effective_diffusivity(sim, problem, :flux, depth = 1)
 
-τ = 1/D_eff_pore #not the same as D_eff continuum
 @info "τ: $(@sprintf("%.5f", τ))"
 
 if PLOT
     t_analytic = range(sim.t[1], sim.t[end], 100)
-    m_analytic = f(t_analytic, fit.param)
+    J_analytic = f(t_analytic, fit.param)
 
-    p = plot(t_analytic, m_analytic,
-        title = "Mass Intake Curve",
+    p = plot(t_analytic, J_analytic,
+        title = "Outlet Flux Curve",
         xlabel = "time",
-        ylabel = "mass intake",
+        ylabel = "outlet flux",
         label = "fitted curve, τ = $(@sprintf("%.5f", τ))",
         legend = :bottomright
     )
