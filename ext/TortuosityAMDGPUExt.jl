3640ede0662331653fddbaa55c7a27853c42d390
Amin Sadeghi <amin.sadeghi@live.com>
2026-09-01 14:54:46 -0700

    fix(amdgpu): retain synchronization barriers

--- ext/TortuosityAMDGPUExt.jl
+++ ext/TortuosityAMDGPUExt.jl
@@ -14,7 +14,6 @@ function __init__()
 end
 
 Tortuosity._on_gpu(::ROCArray) = true
-Tortuosity._async_return_safe(::ROCArray) = true
 
 # Mirror the CPU precompile workload in src/Tortuosity.jl for the ROCm GPU path.
 # Only runs when an AMDGPU device is actually present at extension-precompile
